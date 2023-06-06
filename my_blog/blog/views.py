fe0d5bd226ff6f5d4f65bdaa2d819b6daf35985a
ramzandashkar <dashkarov@gmail.com>
2023-06-06 16:37:54 +0300

    Add send e-mail

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,8 +1,9 @@
-from django.core.paginator import (
-    Paginator,
-    EmptyPage,
-    PageNotAnInteger,
-)
+from django.core.mail import send_mail
+# from django.core.paginator import (
+#     Paginator,
+#     EmptyPage,
+#     PageNotAnInteger,
+# )
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
@@ -33,20 +34,30 @@ class PostListView(ListView):
 
 
 def post_share(request, post_id):
-    post = get_object_or_404(
-        Post,
-        id=post_id,
-        status=Post.Status.PUBLISHED,
-    )
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
     if request.method == 'POST':
+        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
+            # Form fields passed validation
             cd = form.cleaned_data
-        else:
-            form = EmailPostForm()
-        return render(request, 'blog/post/share.html', {'post': post,
-                                                        'form': form,
-                                                        })
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'sirius712@mail.ru',
+                      [cd['to']])
+            sent = True
+
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
 
 
 def post_detail(request, year, month, day, post):
