3f586b18f143c48a7f43a12c23ae89aa941261cc
ramzandashkar <dashkarov@gmail.com>
2023-06-07 12:53:18 +0300

    Add similar post`s tags

--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import (
     EmptyPage,
     PageNotAnInteger,
 )
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
@@ -79,10 +80,16 @@ def post_detail(request, year, month, day, post):
     )
     comments = post.comments.filter(active=True)
     form = CommentForm()
+
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     context = {
         'post': post,
         'comments': comments,
         'form': form,
+        'similar_posts': similar_posts,
     }
 
     return render(request, template, context)
